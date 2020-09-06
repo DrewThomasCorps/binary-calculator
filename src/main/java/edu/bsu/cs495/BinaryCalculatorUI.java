b67ac9dd50180f4fdc94bf03a9baa092062661d7
Alan Bauer <abauerdev@gmail.com>
2020-09-06 12:48:47 -0400

    Add JavaFX and FXML files

---
+++ src/main/java/edu/bsu/cs495/BinaryCalculatorUI.java
@@ -0,0 +1,23 @@
+package edu.bsu.cs495;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BinaryCalculatorUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("binaryCalculatorUI.fxml"));
+        primaryStage.setTitle("Binary Calculator");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
