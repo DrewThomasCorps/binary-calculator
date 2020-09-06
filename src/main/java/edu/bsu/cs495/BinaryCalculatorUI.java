c2c741e54bfccacbf29965e2526a68306074431b
Alan Bauer <abauerdev@gmail.com>
2020-09-06 13:34:14 -0400

    Add resources folder and refactor .fxml location

--- src/main/java/edu/bsu/cs495/BinaryCalculatorUI.java
+++ src/main/java/edu/bsu/cs495/BinaryCalculatorUI.java
@@ -10,7 +10,7 @@ public class BinaryCalculatorUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("binaryCalculatorUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/binaryCalculatorUI.fxml"));
         primaryStage.setTitle("Binary Calculator");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
