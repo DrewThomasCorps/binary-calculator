c686abdafb044e9b28f0450aa8fe37344e6eb8c4
Alan Bauer <abauerdev@gmail.com>
2020-09-07 13:34:23 -0400

    Remove logic from controller

--- src/main/java/edu/bsu/cs495/BinaryCalculatorController.java
+++ src/main/java/edu/bsu/cs495/BinaryCalculatorController.java
@@ -10,56 +10,25 @@ public class BinaryCalculatorController {
 
     @FXML
     private Label resultDisplay;
-    private String firstBinaryDigit = "";
-    private String secondBinaryDigit = "";
-    private String operator = "";
-    private boolean isInStartState = true;
-
 
     public void handleDigit(ActionEvent event) {
 
         if (resultDisplay.getText().length() < 64) {
             String digitValue = ((Button)event.getSource()).getText();
             resultDisplay.setText(resultDisplay.getText() + digitValue);
-            isInStartState = false;
         }
 
     }
 
     public void handleOperator(ActionEvent event) {
 
-        if (!isInStartState && secondBinaryDigit.isEmpty()) {
-
-            clearResultLabel();
-
-            operator = ((Button)event.getSource()).getText();
-
-        } else if (!secondBinaryDigit.isEmpty()) {
-
-            System.out.println("I'm waiting for you to press = or clear");
-
-        } else {
-
-            System.out.println("I'm waiting for a digit");
-
-        }
     }
 
     public void handleEquals() {
 
-        System.out.println("clicked equals");
-        //TODO Implement operation execution
-        binaryCalculator.calculate(firstBinaryDigit);
-        clearOperator();
-        clearDigits();
-        resetStartState();
-
     }
 
     public void handleToggle() {
-
-        System.out.println("clicked toggle");
-        //TODO Implement toggle
 
     }
 
@@ -70,39 +39,19 @@ public class BinaryCalculatorController {
     public void handleClear() {
 
         clearResultLabel();
-        clearDigits();
-        clearOperator();
-        resetStartState();
         binaryCalculator.clear();
 
     }
 
     public void handleSquareRoot() {
 
-        if (operator.isEmpty() && !firstBinaryDigit.isEmpty()) {
-
-            System.out.println("clicked sqrt");
-            binaryCalculator.squareRoot(firstBinaryDigit);
-            //TODO Implement square root operation
-            clearOperator();
-            clearDigits();
-            resetStartState();
-
-        }
+        resultDisplay.setText((binaryCalculator.squareRoot(resultDisplay.getText())).toString());
 
     }
 
     public void handleSquare() {
 
-        if (operator.isEmpty() && !firstBinaryDigit.isEmpty()) {
-
-            resultDisplay.setText((binaryCalculator.square(firstBinaryDigit)).toString());
-
-            clearOperator();
-            clearDigits();
-            resetStartState();
-
-        }
+        resultDisplay.setText((binaryCalculator.square(resultDisplay.getText())).toString());
 
     }
 
@@ -112,18 +61,4 @@ public class BinaryCalculatorController {
 
     }
 
-    public void clearOperator() {
-
-        operator = "";
-
-    }
-
-    public void clearDigits() {
-        firstBinaryDigit = "";
-        secondBinaryDigit = "";
-    }
-
-    public void resetStartState() {
-        isInStartState = true;
-    }
 }
